14c6d42868b8b9be80a42390bb2b0adf72f73f2d
Médéric Delamare <mederic.delamare@trsb.net>
2024-07-19 11:38:04 +0200

    feat: Removing unused components

--- src/components/charts/BilanChart.tsx
+++
@@ -1,8 +0,0 @@
-import { useSimulation } from '@/publicodes-state'
-import RavijenChart from './RavijenChart'
-
-export default function BilanChart() {
-  const { categories, subcategories } = useSimulation()
-
-  return <RavijenChart categories={categories} subcategories={subcategories} />
-}
