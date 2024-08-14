0cb19cc6fc6cdde6260e3282419b9f2e4717d3c1
Médéric Delamare <mederic.delamare@trsb.net>
2024-08-14 15:33:30 +0200

    feat(OW): Setting up an API, consuming the API and writing data to a sheet

--- src/app/(layout-with-navigation)/(simulation)/fin/page.tsx
+++ src/app/(layout-with-navigation)/(simulation)/fin/page.tsx
@@ -1,16 +1,33 @@
 'use client'
 
-import { useEffect } from 'react'
+import {useEffect, useState} from 'react'
 
 export default function FinPage() {
+  const [setData] = useState(null)
 
   useEffect(() => {
+    const value = localStorage.getItem('nosgestesempreinte::v1')
+    fetch('/api/add-row', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: value }),
+    })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log('Réponse du serveur:', data);
+          setData(data.message);
+        })
+        .catch((error) => {
+          console.error('Erreur lors de l\'envoi de la requête:', error);
+        });
     const timer = setTimeout(() => {
       window.location.replace('/');
     }, 10000);
 
     return () => clearTimeout(timer);
-  });
+  }, []);
   return (
     <>
       <div className="relative flex items-center justify-center overflow-hidden p-4 h-[100vh]">
