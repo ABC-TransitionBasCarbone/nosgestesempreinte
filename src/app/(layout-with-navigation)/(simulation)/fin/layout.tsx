b27cbaf762a150f5691fea705c2afaa6848ab405
Chloe M <chloe.molle@neuf.fr>
2024-09-09 17:57:52 +0200

    change windows title

--- src/app/(layout-with-navigation)/(simulation)/fin/layout.tsx
+++ src/app/(layout-with-navigation)/(simulation)/fin/layout.tsx
@@ -1,19 +1,11 @@
 import { noIndexObject } from '@/constants/metadata'
-import { getServerTranslation } from '@/helpers/getServerTranslation'
 import { getMetadataObject } from '@/helpers/metadata/getMetadataObject'
 import { FormProvider } from '@/publicodes-state'
 import { PropsWithChildren } from 'react'
 
 export async function generateMetadata() {
-  const { t } = await getServerTranslation()
-
   return getMetadataObject({
-    title: t(
-      "Vos résultats, simulateur d'empreinte climat - Nos Gestes Climat"
-    ),
-    description: t(
-      "Vos résultats de tests de notre simulateur d'empreinte carbone."
-    ),
+    title: "Enquête 2024",
     robots: noIndexObject,
     alternates: {
       canonical: '/fin',
