42b06114138361af571b4f7102e1a52aa019601d
Chloe M <chloe.molle@neuf.fr>
2024-09-17 17:50:16 +0200

    simplify migration

--- src/helpers/modelFetching/getMigrationInstructions.ts
+++ src/helpers/modelFetching/getMigrationInstructions.ts
@@ -1,9 +1,3 @@
-import { MigrationType } from '@/publicodes-state/types'
-import migration from '@abc-transitionbascarbone/nosgestesempreinte-modele/public/migration.json'
-
-/**
- * This function is used to get the migration instructions. It can be called directly from a server component.
- */
-export async function getMigrationInstructions(): Promise<MigrationType> {
-  return Promise.resolve(migration)
+export function getMigrationInstructions() {
+  return { keysToMigrate: {}, valuesToMigrate: {}}
 }
