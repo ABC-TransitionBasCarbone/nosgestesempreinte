9de7571acf5694cd195b0d65e3700e8c5c890ef3
mdelamare <mederic.delamare@digiwin.fr>
2024-10-15 16:48:24 +0200

    feat: Creation of an api route to retrieve spreadsheet headers + moving intelligence to the front end to use the engine

--- src/components/form/Navigation.tsx
+++ src/components/form/Navigation.tsx
@@ -7,10 +7,11 @@ import {
 import Button from '@/design-system/inputs/Button'
 import { useClientTranslation } from '@/hooks/useClientTranslation'
 import { useMagicKey } from '@/hooks/useMagicKey'
-import { useCurrentSimulation, useRule } from '@/publicodes-state'
+import { useCurrentSimulation, useEngine, useRule } from '@/publicodes-state'
 import { DottedName } from '@/publicodes-state/types'
 
 import { MouseEvent, useCallback, useState } from 'react'
+import { safeEvaluateHelper } from '@/publicodes-state/helpers/safeEvaluateHelper'
 
 type Props = {
   question: DottedName
@@ -35,6 +36,8 @@ export default function Navigation({
   gotoPrevQuestion,
   gotoNextQuestion,
 }: Props) {
+  const { engine } = useEngine()
+
   const { t } = useClientTranslation()
 
   const { isMissing, plancher } = useRule(question)
@@ -45,6 +48,57 @@ export default function Navigation({
     tempValue !== undefined && plancher !== undefined && tempValue < plancher
 
   const [_data, setData] = useState(null);
+  // Fonction pour préparer les données à envoyer
+  const prepareDataToSend = useCallback((JSONValue: any, headers: any[]): Record<string, any>[] => {
+    const dataToSend: any[] = [];
+    const opinionWayId = JSONValue.simulation.id;
+    const simulationData = {
+      ...JSONValue.simulation.situation,
+      ...JSONValue.simulation.suggestions,
+    };
+
+    headers.forEach((key) => {
+      let value = simulationData[key];
+      if (key === 'id opinion way') {
+        dataToSend.push(opinionWayId);
+        return;
+      }
+      if (value === null) {
+        value = 'je ne sais pas';
+      } else if (!key.includes('aide saisie')) {
+        value = safeEvaluateHelper(key, engine)?.nodeValue ?? '';
+      }
+      dataToSend.push(value);
+    });
+
+    return dataToSend;
+  }, [engine]);
+
+  // Fonction pour envoyer les données au serveur
+  const sendDataToServer = useCallback(async (data: any) => {
+    const JSONValue = await getLastSimulationFromLocalStorage();
+    try {
+      const response = await fetch('/api/add-row', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ simulationResults: data, voitures: JSONValue.simulation.voitures }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log('Réponse du serveur:', result);
+      onComplete();
+      setData(result.message);
+    } catch (error) {
+      alert(
+        "Une erreur s'est produite lors de l'enregistrement de votre sondage. Réessayer dans quelques instants ou contactez xx.xx@xx.com"
+      );
+      console.error('Erreur lors de l’envoi des données au serveur:', error);
+    }
+  }, [onComplete]);
 
   const handleGoToNextQuestion = useCallback(
     async (e: KeyboardEvent | MouseEvent) => {
@@ -62,39 +116,15 @@ export default function Navigation({
       handleMoveFocus()
 
       if (noNextQuestion) {
-        const localStorageValue = localStorage.getItem('nosgestesempreinte::v1')
-        let value = null
-        if (localStorageValue) {
-          const JSONValue = JSON.parse(localStorageValue)
-          //TODO: Pour l'instant on prend la dernière mais à voir pour la suite
-          JSONValue.simulation = JSONValue.simulations.at(-1)
-          delete JSONValue.simulations
-          value = JSON.stringify(JSONValue)
-        }
+        const JSONValue = await getLastSimulationFromLocalStorage();
+        if (!JSONValue) return;
 
-        fetch('/api/add-row', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ data: value }),
-        })
-            .then((res) => {
-              if (!res.ok) {
-                throw new Error(`Erreur HTTP: ${res.status}`);
-              }
-              return res.json();
-            })
-            .then((data) => {
-              console.log('Réponse du serveur:', data);
-              onComplete();
-              setData(data.message);
-            })
-            .catch((error) => {
-              alert("Une erreur s'est produite lors de l'enregistrement de votre sondage. Réessayer dans quelques instants ou contactez xx.xx@xx.com")
-              console.error("Erreur lors de l'envoi de la requête:", error);
-            });
-        return
+        const headers = await fetchHeaders();
+        if (!headers) return;
+
+        const dataToSend = prepareDataToSend(JSONValue, headers);
+        await sendDataToServer(dataToSend);
+        return;
       }
 
       gotoNextQuestion()
@@ -104,10 +134,43 @@ export default function Navigation({
       gotoNextQuestion,
       noNextQuestion,
       isMissing,
-      onComplete,
       updateCurrentSimulation,
+      prepareDataToSend,
+      sendDataToServer
     ]
   )
+  async function getLastSimulationFromLocalStorage() {
+    const localStorageValue = localStorage.getItem('nosgestesempreinte::v1');
+    if (!localStorageValue) return null;
+
+    const JSONValue: any = JSON.parse(localStorageValue);
+    JSONValue.simulation = JSONValue.simulations.at(-1); // Dernière simulation
+    delete JSONValue.simulations;
+    return JSONValue;
+  }
+
+  async function fetchHeaders(): Promise<any[]> {
+    try {
+      const response = await fetch('/api/get-headers', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP: ${response.status}`);
+      }
+
+      const data = await response.json();
+      return data.headers || [];
+    } catch (error) {
+      alert(
+        "Une erreur s'est produite lors de l'enregistrement de votre sondage. Réessayer dans quelques instants ou contactez xx.xx@xx.com"
+      );
+      console.error('Erreur lors de la récupération des en-têtes:', error);
+      return [];
+    }
+  }
+  
 
   useMagicKey({
     gotToNextQuestion: handleGoToNextQuestion,
--- src/pages/api/add-row.ts
+++ src/pages/api/add-row.ts
@@ -1,27 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { google } from 'googleapis';
-import { validate as uuidValidate } from 'uuid';
+import { getGoogleSheetsService } from '@/services/googleSheetsService'
 
 type ResponseData = {
   message: string;
 };
 
 const ERROR_MESSAGES = {
-  INVALID_UUID: 'The provided UUID is not valid.',
   MISSING_SPREADSHEET_ID: 'The SPREADSHEET_ID is not defined in the .env.',
   APPEND_ERROR: 'An error occurred while adding the row.',
   APPEND_ERROR_CAR: 'An error occurred while adding the row in car details.',
   ROUTE_NOT_FOUND: 'API route not found',
   HEADERS_ERROR: 'An error occurred while retrieving the headers',
+  BAD_REQUEST: 'Request body could not be read properly.'
 };
 
 const SUCCESS_MESSAGES = {
   SUCCESS: 'The row was successfully added.',
 };
 
-export default async function handler(
+export default async function addRow(
     req: NextApiRequest,
     res: NextApiResponse<ResponseData>
 ) {
@@ -30,60 +29,20 @@ export default async function handler(
   }
 
   try {
-    const data = req.body.data;
+    const simulationResults = req.body.simulationResults;
+    const voitures = req.body.voitures;
 
-    if (!data) {
-      return res.status(400).json({ message: ERROR_MESSAGES.INVALID_UUID });
+    if (!simulationResults || !voitures) {
+      return res.status(400).json({ message: ERROR_MESSAGES.BAD_REQUEST });
     }
-
-    const jsonData = JSON.parse(data);
-    const userId = jsonData.simulation?.opinionWayId;
     const spreadsheetId = process.env.SPREADSHEET_ID;
-    let headers = [];
-
-    if (!userId || !uuidValidate(userId)) {
-      return res.status(422).json({ message: ERROR_MESSAGES.INVALID_UUID });
-    }
 
     if (!spreadsheetId) {
       return res.status(400).json({ message: ERROR_MESSAGES.MISSING_SPREADSHEET_ID });
     }
 
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        type: 'service_account',
-        project_id: process.env.GOOGLE_PROJECT_ID,
-        private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-        private_key: Buffer.from(process.env.GOOGLE_PRIVATE_KEY, 'base64').toString('utf-8'),
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        client_id: process.env.GOOGLE_CLIENT_ID,
-        auth_uri: process.env.GOOGLE_AUTH_URI,
-        token_uri: process.env.GOOGLE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
-      },
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-    });
-    
-    const service = google.sheets({ version: 'v4', auth });
-    try {
-      const response = await service.spreadsheets.values.get({
-        spreadsheetId,
-        range: 'A1:1',
-      });
-      headers = response.data.values[0]
-    }catch (error) {
-      console.error('Append Error:', error);
-      return res.status(500).json({ message: ERROR_MESSAGES.APPEND_ERROR });
-    }
-
-    const valuesMerged = {
-      ...jsonData.simulation.situation,
-      ...jsonData.simulation.suggestions,
-    };
-
-    const values = [mapDataToSheet(valuesMerged, headers, userId)];
-
+    const service = getGoogleSheetsService()
+    const values = [simulationResults];
     const resource = { values };
 
     try {
@@ -98,14 +57,14 @@ export default async function handler(
       return res.status(500).json({ message: ERROR_MESSAGES.HEADERS_ERROR });
     }
 
-    if (jsonData.simulation.voitures) {
+    if (voitures) {
       try {
         await service.spreadsheets.values.append({
           spreadsheetId,
           valueInputOption: 'USER_ENTERED',
           range: "'details-trajet'!A1",
           resource: {
-            values: jsonData.simulation.voitures.map(({ label, opinionWayId, distance, reccurrence, period, passengers }) => [
+            values: voitures.map(({ label, opinionWayId, distance, reccurrence, period, passengers }) => [
               opinionWayId,
               label,
               distance,
@@ -126,20 +85,4 @@ export default async function handler(
     console.error('Handler Error:', err);
     return res.status(500).json({ message: ERROR_MESSAGES.APPEND_ERROR });
   }
-}
-
-function mapDataToSheet(simulationData: Record<string, any>, keys: string[], userId: string): any[][] {
-  const values: any[] = [];
-  keys.forEach(key => {
-    let value = simulationData[key];
-
-    if (value === null) {
-      value = 'je ne sais pas';
-    } else if (value === undefined) {
-      value = '';
-    }
-    values.push(value);
-  });
-  values[0] = userId;
-  return values;
 }
