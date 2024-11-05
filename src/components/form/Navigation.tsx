15a6edb4d8c541c0666c81d527eb6dcf6ff02e88
Chloe M <chloe.molle@neuf.fr>
2024-11-05 09:33:37 +0100

    fix disable button + remove plane input

--- src/components/form/Navigation.tsx
+++ src/components/form/Navigation.tsx
@@ -10,7 +10,7 @@ import { useMagicKey } from '@/hooks/useMagicKey'
 import { useCurrentSimulation, useEngine, useRule } from '@/publicodes-state'
 import { DottedName } from '@/publicodes-state/types'
 
-import { MouseEvent, useCallback, useState } from 'react'
+import { MouseEvent, useCallback, useMemo, useState } from 'react'
 import { safeEvaluateHelper } from '@/publicodes-state/helpers/safeEvaluateHelper'
 
 type Props = {
@@ -44,10 +44,13 @@ export default function Navigation({
 
   const { updateCurrentSimulation } = useCurrentSimulation()
 
-  const isNextDisabled =
-      (tempValue !== undefined && plancher !== undefined && tempValue < plancher) || value === undefined
+  const isNextDisabled = useMemo(() => {
+    return !transitionPage &&
+    ((tempValue !== undefined && plancher !== undefined && tempValue < plancher) || value === undefined);
+  }, [plancher, tempValue, transitionPage, value]);
 
   const [_data, setData] = useState(null);
+
   // Fonction pour préparer les données à envoyer
   const prepareDataToSend = useCallback((JSONValue: any, headers: any[]): Record<string, any>[] => {
     const dataToSend: any[] = [];
